b94072139b1b3bc6e5a460b9f214cd2f050d3acb
dci-student <yasaman.nikpay@gmail.com>
2024-10-22 16:14:54 +0200

    11 commit

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -8,25 +8,49 @@ export default function Category() {
 const {state} = useContext(RecipeContext)
  const [categories , setCategories] = useState([])
 
+  // useEffect(() => {
+  //   console.log("test")
+  //   fetch(`https://www.themealdb.com/api/json/v1/1/categories.php`)
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       setCategories(data.categories)
+  //       window.tns({
+  //         container: ".category-list",
+  //         items: 5,
+  //         slideBy: 1,
+  //         autoplay:true,
+  //         autoplayTimeout: 2000,
+  //         nav: false, // die punkte
+  //         controls: false, // die buttons
+  //         autoplayButtonOutput: false,
+  //       });
+  //     })
+  //     .catch((error) => console.error("Error fetching categories:", error));
+  // }, []);
+
   useEffect(() => {
-    console.log("test")
     fetch(`https://www.themealdb.com/api/json/v1/1/categories.php`)
       .then((response) => response.json())
       .then((data) => {
-        setCategories(data.categories)
-        window.tns({
-          container: ".category-list",
-          items: 5,
-          slideBy: 1,
-          autoplay:true,
-          autoplayTimeout: 2000,
-          nav: false, // die punkte
-          controls: false, // die buttons
-          autoplayButtonOutput: false,
-        });
+        setCategories(data.categories);
       })
       .catch((error) => console.error("Error fetching categories:", error));
   }, []);
+
+  useEffect(() => {
+    if (categories.length > 0) {
+      window.tns({
+        container: ".category-list",
+        items: 5,
+        slideBy: 1,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        nav: false, // Dots
+        controls: false, // Buttons
+        autoplayButtonOutput: false,
+      });
+    }
+  }, [categories]);
 
  
 
